d71affc9b143f5f8de9651f27dccc5c032f6993b
fxedel <fxedel@gmail.com>
2016-09-21 01:40:41 +0200

    Added simple ecue and qlcplus export tests (#19)
    Added simple ecue and qlcplus export tests
    
    * added diff devDependency
    
    * fixed path issues
    
    * run script with node instead of itself directly
    
    * fix node command
    
    * removed author tag from diff
    
    * make tests work for node v4

--- tests/fixtures_convert_test.js
+++ tests/fixtures_convert_test.js
@@ -2,11 +2,78 @@
 
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const cp = require('child_process');
+const diff = require('diff');
 
-console.log("Testing will be implemented soon. But let's try calling the diff command.");
+const assert = require('assert');
 
-const returnVal = cp.spawnSync('diff', ['../README.md', '../package.json'], {encoding: 'utf8'});
-console.log(returnVal.output);
+const timestamp = new Date().toISOString().replace(/T/, '_').replace(/:/g, '-').replace(/\..+/, '');
+const outputDir = path.join(__dirname, `out-${timestamp}`);
+fs.mkdir(outputDir, (err) => {
+    if (err) throw err;
 
-process.exit(0);
+    test(
+        'ADJ Quad Phase HP',
+        'qlcplus',
+        'adj_quad_phase_hp.json',
+        'American-DJ-Quad-Phase-HP.qxf'
+    );
+    test(
+        'Eurolite LED KLS-801',
+        'ecue',
+        'eurolite_led_kls-801.json',
+        'UserLibrary.xml',
+        'Eurolite-LED-KLS-801.xml'
+    );
+});
+
+
+function test(name, format, inputFile, desiredOutputFile, outputFile) {
+    if (outputFile === undefined) {
+        outputFile = desiredOutputFile;
+    }
+
+    cp.exec(
+        'node ' + path.join(__dirname, '..', 'fixtures_convert.js')
+        + ' -i ' + path.join(__dirname, 'fixtures', inputFile)
+        + ` -f ${format} -d ` + path.join(outputDir, '%FORMAT%'),
+        (error, stdout, stderr) => {
+            console.log(`Testing: Convert ${name} with format ${format} ...`);
+
+            if (error) {
+                console.error(error);
+                return;
+            }
+            if (stderr) {
+               console.log(stderr);
+            }
+
+            const outFilePath = path.join(outputDir, format, desiredOutputFile);
+            const desiredOutFilePath = path.join(__dirname, 'desired_out', format, outputFile);
+
+            let out = fs.readFileSync(outFilePath, 'utf8');
+            let desiredOut = fs.readFileSync(desiredOutFilePath, 'utf8');
+
+            if (format == 'ecue') {
+                out = out.replace(/\d\d\d\d\-\d\d\-\d\d#\d\d:\d\d:\d\d/g, '');
+                desiredOut = desiredOut.replace(/\d\d\d\d\-\d\d\-\d\d#\d\d:\d\d:\d\d/g, '');
+            }
+            else if (format == 'qlcplus') {
+                out = out.replace(/<Author>[^<]*<\/Author>/g, '<Author></Author>');
+                desiredOut = desiredOut.replace(/<Author>[^<]*<\/Author>/g, '<Author></Author>');
+            }
+
+            try {
+                assert(out == desiredOut);
+            }
+            catch(assertionError) {
+                console.error("Test failed: Out file doesn't equal desired out.");
+                console.log(diff.createTwoFilesPatch(outFilePath, desiredOutFilePath, out, desiredOut));
+                process.exit(1);
+            }
+            console.log('Test ok.')
+        }
+    );
+}
