b21a77d2b63b28b2933322255618be2f42ae66fc
Florian Edelmann <florian-edelmann@online.de>
2016-09-27 23:29:03 +0200

    remove Array.includes polyfill

--- formats/qlcplus.js
+++ formats/qlcplus.js
@@ -67,7 +67,7 @@ module.exports.export = function formatQLCplus(manufacturers, fixtures, optionOu
             else if (chData.type == 'Strobe') {
                 chData.type = 'Shutter';
             }
-            else if (!allowedChannelTypes.includes(chData.type)) {
+            else if (allowedChannelTypes.indexOf(chData.type) == -1) {
                 console.warn(`Channel type "${chData.type}" not supported, falling back to "Intensity" (in "${fixData.name}", channel "${chData.name}").`);
                 chData.type = 'Intensity';
             }
@@ -364,39 +364,4 @@ function die(errorStr, logStr) {
     if (logStr)
         console.log(logStr);
     process.exit(1);
-}
-
-
-// polyfill to support Node v5 and below
-if (!Array.prototype.includes) {
-  Array.prototype.includes = function(searchElement /*, fromIndex*/) {
-    'use strict';
-    if (this == null) {
-      throw new TypeError('Array.prototype.includes called on null or undefined');
-    }
-    
-    var O = Object(this);
-    var len = parseInt(O.length, 10) || 0;
-    if (len === 0) {
-      return false;
-    }
-    var n = parseInt(arguments[1], 10) || 0;
-    var k;
-    if (n >= 0) {
-      k = n;
-    } else {
-      k = len + n;
-      if (k < 0) {k = 0;}
-    }
-    var currentElement;
-    while (k < len) {
-      currentElement = O[k];
-      if (searchElement === currentElement ||
-         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
-        return true;
-      }
-      k++;
-    }
-    return false;
-  };
 }
